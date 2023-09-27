245f7e08ae9cb7c5d1360c4730e7512a84ede07f
karikey rana <rana.kartikey999@gmail.com>
2023-09-27 09:13:30 +0530

    removing strict mode

--- client/socketIO/src/App.jsx
+++ client/socketIO/src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef, createElement } from 'react'
 import './App.css'
 import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3002', ()=>console.log('connected'));
 function App() {
-  const [msg, setMsg] = useState()
+  const [msg, setMsg] = useState();
+  const [list, setList] = useState([]);
   const dispatchMessage = () =>{
     socket.emit('send_message', {message : msg});
   }
+  useEffect(()=>{
+      socket.on('recieve_message', (data)=>{
+        setList(p=>[...p, data.message]);
+      })
+  },[socket]);
+  console.log(list);
   return (
     <>
       <div>
-        <input value = {msg} onChange={(e)=>setMsg(e.target.value)}></input>
+        <input onChange={(e)=>setMsg(e.target.value)}></input>
         <button onClick={dispatchMessage}>Send Message</button>
+        <div>
+          {list.map((obj, idx)=>{
+            return <li key={idx}>{obj}</li>
+          })}
+        </div>
       </div>
     </>
   )
